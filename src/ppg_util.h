bd99678d74877d28ce23bb43e958d73f8eb4e9c5
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-01-12 14:20:26 +0100

    Added PANIC macro

--- src/ppg_util.h
+++ src/ppg_util.h
@@ -12,3 +12,13 @@
 #endif
 
 double *ppg_util_generate_random_data(size_t size);
+
+#define PANIC(fmt, ...)                                                                                   \
+    do {                                                                                                  \
+        fprintf(stderr, "[[DS-PANIC]] function %s (file: %s, line: %d): ", __func__, __FILE__, __LINE__); \
+        fprintf(stderr, fmt, ##__VA_ARGS__);                                                              \
+        fprintf(stderr, "\n");                                                                            \
+        abort();                                                                                          \
+    } while (0)
+#define PANIC_STR(msg) PANIC("%s", msg)
+#define PANIC_MALLOC() PANIC_STR("FAILED TO MALLOC")
